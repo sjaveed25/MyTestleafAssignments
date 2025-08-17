6bcc2ae3fb380690d03d3b0c97f0262262281035
sjaveed25 <sjaveed25@gmail.com>
2025-08-17 11:51:01 +0530

    Add files via upload
    Day 1

---
+++ testLeafHomeTraining/day1/Employeedetails.java
@@ -0,0 +1,31 @@
+package testLeafHomeTraining.day1;
+
+public class Employeedetails {
+
+	public static void main(String[] args) {
+
+
+		String name = "John Doe";
+		short age = 30;
+		double salary  = 55000.75;
+		char gender = 'M';
+		boolean acceptTerms = true;
+		float rating = 4.5f;
+		long mobileNumber = 9876543210L;
+		int pinCode = 56001;
+		int luckyNumber = 7;
+		
+		System.out.println("name is " + name);
+		System.out.println("age is " + age);
+		System.out.println("salary is " + salary);
+		System.out.println("gender is " + gender);
+		System.out.println("acceptTerms is " + acceptTerms);
+		System.out.println("rating is " + rating);
+		System.out.println("mobileNumber is " + mobileNumber);
+		System.out.println("pinCode is " + pinCode);
+		System.out.println("luckyNumber is " + luckyNumber);
+		
+		
+	}
+
+}
